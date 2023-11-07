620f43b30354567377def14e542daa9de927b112
Miguel Carrasco <quintes32-miguel@hotmail.es>
2023-11-07 21:22:21 +0100

    Conexion a base de datos

--- src/main/java/com/is/gestionterrenos/dao/ConexionDB.java
+++ src/main/java/com/is/gestionterrenos/dao/ConexionDB.java
@@ -1,5 +1,21 @@
 package com.is.gestionterrenos.dao;
+import java.sql.*;
 
 public class ConexionDB {
+    public static void main(String[] args) {
+        try {
+            Connection miConexion=DriverManager.getConnection("jdbc:mysql://localhost:3306/balonmano", "root", "password");
+			
+			/*PreparedStatement miStatement=miConexion.prepareStatement("SELECT DISTINCTROW nombre_club from equipo");
+			ResultSet rs=miStatement.executeQuery();
+			int n=0;
+			while(rs.next()){
+				n++;
+			}*/
+        } catch (SQLException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
     
 }
