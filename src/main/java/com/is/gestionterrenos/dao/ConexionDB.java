4d1d97f5e2a0e07e350e70e79de1d70b9b003051
Miguel Carrasco <quintes32-miguel@hotmail.es>
2023-11-09 17:03:25 +0100

    Update ConexionDB.java

--- src/main/java/com/is/gestionterrenos/dao/ConexionDB.java
+++ src/main/java/com/is/gestionterrenos/dao/ConexionDB.java
@@ -3,13 +3,15 @@ import java.sql.*;
 
 //TODO: Terminar conexionDB
 public class ConexionDB {
-    public static void main(String[] args) {
+
+    public Connection getConn() {
         try {
             //CAMBIAR "password" por vuestra contraseña en mySql
             Connection miConexion=DriverManager.getConnection("jdbc:mysql://localhost:3306/gestionterrenos", "root", "password");
 			
-			PreparedStatement miStatement=miConexion.prepareStatement("INSERT INTO parcelas VALUES (1,null,'treinta', 'limite')");
-			miStatement.executeUpdate();
+            return miConexion;
+			//PreparedStatement miStatement=miConexion.prepareStatement("INSERT INTO parcelas VALUES (1,null,'treinta', 'limite')");
+			//miStatement.executeUpdate();
 			/*int n=0;
 			while(rs.next()){
 				n++;
@@ -18,6 +20,7 @@ public class ConexionDB {
             // TODO Auto-generated catch block
             e.printStackTrace();
         }
+        return null;
     }
     
 }
