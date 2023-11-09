882be776e3b78466fcbf14ed0d6c4ee812d255cc
Miguel Carrasco <quintes32-miguel@hotmail.es>
2023-11-09 20:35:55 +0100

    Funcionalidad Añadir Arrendatarios

--- src/main/java/com/is/gestionterrenos/controlador/ControladorArrendatarios.java
+++ src/main/java/com/is/gestionterrenos/controlador/ControladorArrendatarios.java
@@ -1,5 +1,19 @@
 package com.is.gestionterrenos.controlador;
+import com.is.gestionterrenos.dao.ArrendatarioDAO;
+import com.is.gestionterrenos.modelo.Arrendatario;
+import com.is.gestionterrenos.vista.VistaArrendatarios;
+
+import java.sql.Date;
+import java.util.ArrayList;
+
 
 public class ControladorArrendatarios {
+
+    public static void insertar() {
+        ArrendatarioDAO.insertar(VistaArrendatarios.dniActual,VistaArrendatarios.nombreActual,Integer.parseInt(VistaArrendatarios.edadActual),VistaArrendatarios.sexoActual, new Date(System.currentTimeMillis()));
+    }
     
+    public static ArrayList<Arrendatario> listar(){
+        return ArrendatarioDAO.listarTodos();
+    }
 }
--- src/main/java/com/is/gestionterrenos/vista/VistaArrendatarios.java
+++ src/main/java/com/is/gestionterrenos/vista/VistaArrendatarios.java
@@ -2,6 +2,7 @@ package com.is.gestionterrenos.vista;
 
 import javax.swing.*;
 
+import com.is.gestionterrenos.controlador.ControladorArrendatarios;
 import com.is.gestionterrenos.modelo.Arrendatario;
 
 import java.awt.*;
@@ -9,13 +10,21 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.ArrayList;
+import java.util.Date;
 
-class VistaArrendatarios {
+public class VistaArrendatarios {
     private JPanel panel;
     private DefaultListModel<String> listModel;
     private JList<String> jList;
 
-    public VistaArrendatarios() {
+    public static String dniActual;
+    public static String nombreActual;
+    public static String edadActual;
+    public static String sexoActual;
+
+
+    public  VistaArrendatarios() {
         panel = new JPanel(new BorderLayout());
 
         listModel = new DefaultListModel<>();
@@ -28,6 +37,17 @@ class VistaArrendatarios {
         JButton actButton = new JButton("Actualizar");
         JButton buscarButton = new JButton("Buscar");
         JButton deleteButton = new JButton("Borrar");
+
+         addButton.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // TODO Auto-generated method stub
+                abrirVentanaAñadir();
+            }
+
+            
+         });
 
         botonesPanel.add(addButton);
         botonesPanel.add(actButton);
@@ -45,13 +65,59 @@ class VistaArrendatarios {
         listModel.clear();
 
         // SE LLAMA AL CONTROLADOR PARA OBTENER LOS ARRENDATARIOS
-        Arrendatario[] arrendatarios=null;
+        ArrayList<Arrendatario> arrendatarios=ControladorArrendatarios.listar();
         //arrendatarios=...
         if (arrendatarios != null) {
             for (Arrendatario arrendatario : arrendatarios) {
                 listModel.addElement(arrendatario.toString());
             }
         }
+    }
+
+    private void abrirVentanaAñadir() {
+        final JFrame ventanaAñadir = new JFrame("Añadir Arrendatario");
+        ventanaAñadir.setSize(300, 200);
+        ventanaAñadir.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+
+        final JPanel panelAñadir = new JPanel(new GridLayout(8, 4));
+
+        panelAñadir.add(new JLabel("DNI:"));
+        final JTextField dniField = new JTextField();
+        panelAñadir.add(dniField);
+
+        panelAñadir.add(new JLabel("Nombre:"));
+        final JTextField nombreField = new JTextField();
+        panelAñadir.add(nombreField);
+
+        panelAñadir.add(new JLabel("Edad:"));
+        final JTextField edadField = new JTextField();
+        panelAñadir.add(edadField);
+
+        panelAñadir.add(new JLabel("Sexo:"));
+        final JTextField sexoField = new JTextField();
+        panelAñadir.add(sexoField);
+
+        JButton guardarButton = new JButton("Guardar");
+        guardarButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // Aquí puedes obtener los datos ingresados y realizar la lógica de guardado
+                 dniActual = dniField.getText();
+                nombreActual = nombreField.getText();
+                edadActual = edadField.getText();
+                sexoActual = sexoField.getText();
+
+                ControladorArrendatarios.insertar();
+                // Cerrar la ventana después de guardar
+                ventanaAñadir.dispose();
+                actualizar();
+            }
+        });
+
+        panelAñadir.add(guardarButton);
+
+        ventanaAñadir.getContentPane().add(panelAñadir);
+        ventanaAñadir.setVisible(true);
     }
 
  
